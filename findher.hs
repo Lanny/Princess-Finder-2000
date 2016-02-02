27a67e8fd29a91e9a87d856fb58000e43c505816
Ryan Jenkins <rjenkins@mail.sfsu.edu>
2016-02-01 23:30:36 -0800

    Added brute force solution finder.

--- findher.hs
+++ findher.hs
@@ -12,4 +12,18 @@ possibleRooms n moves = possibleRoomsI n moves [0..n]
 possibleRoomsI :: Int -> [Int] -> [Int] -> [Int]
 possibleRoomsI n [] priorRooms = priorRooms
 possibleRoomsI n (m:ms) priorRooms = 
-  possibleRoomsI n ms (((filter (notEq m)) . nub . concat . (map (adjacentRooms n))) priorRooms)
+  let newRooms = (filter (notEq m)) . nub . concat . (map (adjacentRooms n)) $ priorRooms
+  in possibleRoomsI n ms newRooms
+
+allOfLen :: Int -> Int -> [[Int]]
+allOfLen n 0 = []
+allOfLen n 1 = [[d] | d <- [0..n]]
+allOfLen n l =
+  let priors = allOfLen n (pred l)
+  in concat [[n : p | n <- [0..n]] | p <- priors]
+
+allMoveSets n = concat [allOfLen n l | l <- [0..]]
+
+proc n (x:xs) = if null (possibleRooms n x) then x else proc n xs
+
+minimalSolution n = proc n (allMoveSets n)
